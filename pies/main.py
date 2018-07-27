dd88b2bc025d94a712c993d337aade46ffa1f75b
Eric Larssen <eric.larssen@workiva.com>
2018-07-27 09:53:03 -0500

    Missed one

--- pies/main.py
+++ pies/main.py
@@ -140,7 +140,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
-    return redirect(url_for('index'))
+    return redirect(url_for('index', _scheme='https'))
 
 
 @app.route('/user')
